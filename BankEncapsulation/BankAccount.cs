1629bab811992ab4bfe23a07f7e03b5bb573f618
Christine Godfrey <cldonnen2001@yahoo.com>
2021-03-24 21:26:32 -0500

    initial push

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;  //  A private field of type double named balance with a value of 0
+
+        public void Deposit(double amount)  //Define a method named Deposit that will accept a double and store that value in the balance field
+        {
+            Console.WriteLine($"Your desposit of {amount} is recieved.");
+            _balance = amount;
+        }
+
+        public double GetBalance()  //Define a method named GetBalance that will return the amount stored in the balance field 
+        {
+            return _balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();     //4.	In the main method of your application, create a new instance of the BankAccount class
+
+            Console.WriteLine("How much are you depositting?");
+            double amountDeposited = double.Parse(Console.ReadLine()); // double.Parse because of way person might type amount of deposit
+
+            ba.Deposit(amountDeposited);                       // Deposit from BankAccount class
+            double startBalance = ba.GetBalance();              // GetBalance from BankAccount class, store into variable "userBalance"
+
+            Console.WriteLine($"Your current balance is now:");
+            Console.WriteLine(amountDeposited + startBalance);
+        }
+    }
+}
